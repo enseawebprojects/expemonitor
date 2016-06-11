62c3ea1990e0224ffeb2c48cb4f8ebf2f28fb269
unknown <karl@.(none)>
2016-06-11 19:39:16 +0200

    test du formulaire d'envoie

---
+++ upload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<meta charset="utf-8">
+
+	<title> Ensea project </title>
+  </head>
+  <body>
+
+<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['data']; ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="project.php">clique ici</a> pour revenir à la page project.php.</p>
+</form>
+  </body>
+</html>
