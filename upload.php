002b2cd05f91316f58149f8ac829bb558bd1af61
unknown <karl@.(none)>
2016-06-12 11:36:11 +0200

    test

--- upload.php
+++ upload.php
@@ -5,8 +5,30 @@
 	
 	<title> Ensea project </title>
 	<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
+	<?php
+// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+if (isset($_FILES['data_recu']) AND $_FILES['data_recu']['error'] == 0)
+{
+        // Testons si le fichier n'est pas trop gros
+        if ($_FILES['data_recu']['size'] <= 1000000)
+        {
+                // Testons si l'extension est autorisée
+                $infosfichier = pathinfo($_FILES['data_recu']['name']);
+                $extension_upload = $infosfichier['extension'];
+                $extensions_autorisees = array('csv','tsv');
+                if (in_array($extension_upload, $extensions_autorisees))
+                {
+                        // On peut valider le fichier et le stocker définitivement
+                        move_uploaded_file($_FILES['data_recu']['tmp_name'], 'upload/' . basename($_FILES['data_recu']['name']));
+                        echo "L'envoi a bien été effectué !";
+                }
+        }
+}
+?>
+	
 	<script> 
-	var data=data_recu;
+	var data=basename($_FILES['data_recu']['name']);
 	function load_data(){
 		d3.csv("data.csv",function(data){
 		console.log(data[0]);
