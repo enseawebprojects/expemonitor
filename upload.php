39fdf8a0e55cf56e42c379f60d7afee8e7924f3e
unknown <karl@.(none)>
2016-06-12 13:30:22 +0200

    test

--- upload.php
+++ upload.php
@@ -2,8 +2,7 @@
 <html>
   <head>
 <meta charset="utf-8">
-	
-	<title> Ensea project </title>
+<title> Ensea project </title>
 <?php
 // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
 if (isset($_FILES['data_recu']) AND $_FILES['data_recu']['error'] == 0)
@@ -18,16 +17,22 @@ if (isset($_FILES['data_recu']) AND $_FILES['data_recu']['error'] == 0)
                 if (in_array($extension_upload, $extensions_autorisees))
                 {
                         // On peut valider le fichier et le stocker définitivement
-                        move_uploaded_file($_FILES['data_recu']['tmp_name'], 'upload/' . basename($_FILES['data_recu']['name']));
+                        move_uploaded_file($_FILES['data_recu']['tmp_name'], '/upload' . basename($_FILES['data_recu']['name']));
                         echo "L'envoi a bien été effectué !";
                 }
         }
-        echo "il y a peut être un problème";
+        
 }
- echo "il y a un problème";
+else{
+     echo "il y a un problème";
+    }
+}
+
+
 ?>
 	
   </head>
+
   <body>
 
 <p>Bonjour !</p>
